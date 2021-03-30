380b2212600650d6deb990419ef3807f8fa10df0
rahcode7 <rahul008mehta@gmail.com>
2021-03-30 14:35:12 +0530

    Cleaned code

--- src/SRL_NN_Classifier.py
+++ src/SRL_NN_Classifier.py
@@ -16,36 +16,14 @@ class SRL_LSTM(nn.Module):
 
         self.fc = nn.Linear(hidden_dim,num_class)
 
-        #self.dropout = nn.Dropout(dropout)
-
-        #self.act = nn.Sigmoid()
-
 
     def forward(self,pretrained_embeddings):
         batch_size = pretrained_embeddings.size(1)
-        #self.hidden = self.init_hidden()
-        #print(batch_size)
-
-
-        #pretrained_embeddings = torch.from_numpy(pretrained_embeddings).float()
-        #pretrained_embeddings
-        
+       
         outputs, (hidden, cell) = self.lstm(pretrained_embeddings)
-        # print(outputs.shape)
-        # print(hidden.shape)
-        # print(cell.shape)
-
-
-        #outputs,_ = torch.nn.utils.rnn.pad_packed_sequence(outputs, batch_first=True)
-        #print(outputs.shape)
-
-
-        #hidden = torch.cat((hidden[-2,:,:], hidden[-1,:,:]), dim = 1)
-        
+       
         outputs = self.fc(outputs)
-        #outputs=self.act(dense_outputs)
-        #print(outputs.shape)
-        #outputs= outputs.view(batch_size, -1)
+     
 
         outputs = outputs.view(batch_size, 23)
 
